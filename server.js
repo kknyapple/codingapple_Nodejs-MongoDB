89b1e7a4096a464c568e423eccd8564f6856cd64
kknyapple <kknyapple@gmail.com>
2022-08-05 20:24:54 +0900

    db에 자료 저장하기

--- server.js
+++ server.js
@@ -6,13 +6,37 @@ app.use(express.urlencoded({ extended: true }));
 
 const MongoClient = require("mongodb").MongoClient;
 
+let db;
+
 MongoClient.connect(
   "mongodb+srv://mongo:mongo123@cluster0.cxma51e.mongodb.net/?retryWrites=true&w=majority",
-  function (에러, client) {
-    if (에러) return console.log(에러);
+  function (err, client) {
+    if (err) return console.log(err);
+
+    db = client.db("todoapp");
+
+    db.collection("post").insertOne(
+      { 이름: "John", 나이: 20, _id: 2 },
+      function (에러, 결과) {
+        console.log("저장완료");
+      }
+    );
+
     //서버띄우는 코드 여기로 옮기기
     app.listen("8080", function () {
       console.log("listening on 8080");
+    });
+
+    // 누가 /add로 post요청하면 데이터 전송
+    // write로 가서 submit하면 db에 저장됨
+    app.post("/add", function (req, res) {
+      res.send("전송완료");
+      db.collection("post").insertOne(
+        { _id: 1, 제목: req.body.title, 날짜: req.body.date },
+        function () {
+          console.log("저장완료");
+        }
+      );
     });
   }
 );
@@ -40,7 +64,8 @@ app.get("/write", function (req, res) {
 //post요청
 //url은 html에 있음 //action="/add" method="POST"
 //req에 input에 적은 정보 저장됨(body-parser 필요)
-app.post("/add", function (req, res) {
-  res.send("전송완료");
-  console.log(req.body);
-});
+// app.post("/add", function (req, res) {
+//   res.send("전송완료");
+//   console.log(req.body.data);
+//   console.log(req.body.title);
+// });
